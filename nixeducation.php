b4825518f7daf75816dda74156417e9795eccf79
Iryna <63670640+ira4356@users.noreply.github.com>
2020-10-30 20:46:41 +0200

    Add files via upload
    Добавлена ссылка на первый коммит

---
+++ nixeducation.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <link rel="https://fonts.googleapis.com/css?family=Montserrat:400,500,700|Old+Standard+TT&display=swap&subset=cyrillic" rel="stylesheet">
+        <title>NIX Education</title>
+    </head>
+    <body>
+        <header class="page-header">
+            <div class="container">
+                <h1 class="heading">Hello, NIX Education!</h1>
+                <h2>Уроки для всех желающих</h2>
+            </div>
+        </header>
+
+        <nav class="navigation">
+            <div class="container">
+                <h2 class="subheading">Меню</h2>
+                <a class="button" href="https://github.com/ira4356/nix/commit/b7c5c949cf563f9452e0e8c6302999c38ae6b226">Commit</a>
+                <br><br>
+                <h3><a title="Ссылка-заглушка">Урок 1</a></h3>
+                <br>
+                <h3><a title="Ссылка-заглушка">Урок 2</a></h3>
+            </div>
+        </nav>
+
+
+
+        <main class="intro">
+            <div class="container">
+                <h2 class="subheading">Первое задание</h2>
+                <figure class="image">
+                    <img src="img/NIX.png"
+                </figure>
+                <h3>Проделанная работа</h3>
+                <p>Сделаны основные блоки сайта, добавлено меню, сделан коммит.</p>
+            </div>
+
+
+        </main>
+
+        <footer class="page-footer">
+            <div class="container">
+                <a class="footer-email" href="mailto:tsaruk4356@gmail.com">tsaruk4356@gmail.com</a>
+            </div>
+        </footer>
+    </body>
+</html>
